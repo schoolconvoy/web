2e6d709515fda8162142d0bc29db182d752ddbfd
Samuel Olaegbe <olaegbesamuel@gmail.com>
2024-04-26 00:13:58 +0100

    View logs if you have permission or are super admin

--- app/Http/Middleware/LogMiddleware.php
+++ app/Http/Middleware/LogMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,7 +17,9 @@ class LogMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (Auth::check() && Auth::user()->hasPermissionTo('view-logs')) {
+        if (Auth::check()
+            &&
+            (Auth::user()->hasAnyRole([User::$SUPER_ADMIN_ROLE]) || Auth::user()->hasPermissionTo('view-logs'))) {
             return $next($request);
         }
 
