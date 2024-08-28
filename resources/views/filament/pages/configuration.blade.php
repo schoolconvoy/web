4c55fc6e920d849b7b133a34b31dc30805d1c033
Samuel Olaegbe <olaegbesamuel@gmail.com>
2024-08-28 08:40:07 +0100

    Remove terms tab from configuratoin

--- resources/views/filament/pages/configuration.blade.php
+++ resources/views/filament/pages/configuration.blade.php
@@ -5,18 +5,18 @@
                 Session
             </x-filament::tabs.item>
 
-            <x-filament::tabs.item @click="tab = 'tab2'" :alpine-active="'tab === \'tab2\''">
+            {{-- <x-filament::tabs.item @click="tab = 'tab2'" :alpine-active="'tab === \'tab2\''">
                 Term
-            </x-filament::tabs.item>
+            </x-filament::tabs.item> --}}
         </x-filament::tabs>
 
         <div class="h-96 w-3/4">
             <div x-show="tab === 'tab1'">
                 @livewire('session-page')
             </div>
-            <div x-show="tab === 'tab2'">
+            {{-- <div x-show="tab === 'tab2'">
                 @livewire('term.create-term')
-            </div>
+            </div> --}}
         </div>
     </div>
 
