e3d279797926735af36cecdc8e1eeddf371a8d22
Samuel Olaegbe <olaegbesamuel@gmail.com>
2025-04-15 00:33:00 +0100

    Fix parent fee and add results page

--- app/Filament/Parent/Resources/FeeResource/Widgets/FeeStatsOverview.php
+++ app/Filament/Parent/Resources/FeeResource/Widgets/FeeStatsOverview.php
@@ -29,6 +29,34 @@ class FeeStatsOverview extends BaseWidget
     protected function getStats(): array
     {
         $ward = User::find(Cache::get('ward'));
+
+        if (!$ward) {
+            return [
+                Stat::make(
+                    'Total fees (after discounts & waivers)',
+                    'NGN 0.00'
+                )
+                ->description('Total amount after applying discounts, waivers and scholarships.')
+                ->icon('heroicon-m-calculator')
+                ->color('gray'),
+                Stat::make(
+                    'Amount paid',
+                    'NGN 0.00'
+                )
+                ->description('Total amount paid so far.')
+                ->icon('heroicon-m-banknotes')
+                ->color('gray'),
+                Stat::make(
+                    'Amount pending',
+                    'NGN 0.00'
+                )
+                ->description('Amount remaining to be paid.')
+                ->icon('heroicon-m-clock')
+                ->color('gray')
+                ->chart([0, 0]),
+            ];
+        }
+
         $totalAmount = User::getOverallAmountWithDiscounts($ward);
         $paidAmount = $ward->payments()->sum('amount');
         $pendingAmount = max(0, $totalAmount - $paidAmount);
